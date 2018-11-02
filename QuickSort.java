67fa51db3b517830cc98346478c209719847b8b4
Gopal Menon <gopal.mnn@gmail.com>
2018-11-01 22:37:33 -0600

    Finished coding

--- QuickSort.java
+++ QuickSort.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
 
 /**
  * QuickSort implementation based on CLRS pseudocode
@@ -5,18 +8,23 @@
  */
 public class QuickSort {
 	
-	/**
-	 * @param arrayToSort
-	 * @param firstElementIndex
-	 * @param lastElementIndex
-	 */
-	public static void sort(int[] arrayToSort, int firstElementIndex, int lastElementIndex) {
+	public static void sort(int[] arrayToSort, int firstElementIndex, int lastElementIndex, boolean singleRandomPivot, int pivotCandidatesParam, Random randomNumberGenerator) {
 		
 		if (firstElementIndex < lastElementIndex) {
 			
+			int pivotIndex = 0;
+			
+			if (singleRandomPivot) {
+				pivotIndex = getRandomPivotIndex(arrayToSort, firstElementIndex, lastElementIndex, randomNumberGenerator);
+			} else {
+				pivotIndex = getPivotIndexFromMedianOfElements(arrayToSort, firstElementIndex, lastElementIndex, pivotCandidatesParam, randomNumberGenerator);
+			}
+			
+			swap(arrayToSort, pivotIndex, lastElementIndex);
+			
 			int partitionIndex = partitionInputArray(arrayToSort, firstElementIndex, lastElementIndex);
-			sort(arrayToSort, firstElementIndex, partitionIndex - 1);
-			sort(arrayToSort, partitionIndex + 1, lastElementIndex);
+			sort(arrayToSort, firstElementIndex, partitionIndex - 1, singleRandomPivot, pivotCandidatesParam, randomNumberGenerator);
+			sort(arrayToSort, partitionIndex + 1, lastElementIndex, singleRandomPivot, pivotCandidatesParam, randomNumberGenerator);
 			
 		}
 		
@@ -37,18 +45,86 @@ public class QuickSort {
 			
 			if (arrayToSort[j] <= pivot) {
 				i += 1;
-				int tempInt = arrayToSort[i];
-				arrayToSort[i] = arrayToSort[j];
-				arrayToSort[j] = tempInt;
+				swap(arrayToSort, i, j);
 			}
 			
 		}
 		
-		int tempInt = arrayToSort[i + 1];
-		arrayToSort[i + 1] = arrayToSort[lastElementIndex];
-		arrayToSort[lastElementIndex] = tempInt;
+		swap(arrayToSort, i + 1, lastElementIndex);
 		
 		return i + 1;
+		
+	}
+	
+	/**
+	 * @param arrayToSort
+	 * @param swapIndex1
+	 * @param swapIndex2
+	 */
+	private static void swap(int[] arrayToSort, int swapIndex1, int swapIndex2) {
+		
+		if (swapIndex1 != swapIndex2) {
+			int tempInt = arrayToSort[swapIndex1];
+			arrayToSort[swapIndex1] = arrayToSort[swapIndex2];
+			arrayToSort[swapIndex2] = tempInt;
+		}
+		
+	}
+	
+	private static int getPivotIndexFromMedianOfElements(int[] arrayToSort, int firstElementIndex, int lastElementIndex, int pivotCandidatesParam, Random randomNumberGenerator) {
+		
+		int numberOfPivotCandidate = 2 * pivotCandidatesParam + 1;
+		
+		if (lastElementIndex - firstElementIndex + 1 <= numberOfPivotCandidate) {
+			return lastElementIndex;
+		}
+		
+		Element[] pivotsArray = new Element[numberOfPivotCandidate];
+		int randomIndex = 0;
+		
+		for (int indexCounter = 0; indexCounter < pivotsArray.length; ++indexCounter) {
+			randomIndex = getRandomPivotIndex(arrayToSort, firstElementIndex, lastElementIndex, randomNumberGenerator);
+			pivotsArray[indexCounter] = new Element(randomIndex, arrayToSort[randomIndex]);
+		}
+		
+		Arrays.sort(pivotsArray, new ElementComparator());
+		return pivotsArray[pivotCandidatesParam].getElementIndex();
+		
+	}
+	
+	private static int getRandomPivotIndex(int[] arrayToSort, int firstElementIndex, int lastElementIndex, Random randomNumberGenerator) {
+		return firstElementIndex + randomNumberGenerator.nextInt(lastElementIndex - firstElementIndex + 1);
+	}
+	
+	private static class Element {
+		private int elementIndex, elementValue;
+		public Element(int elementIndex, int elementValue) {
+			this.elementIndex = elementIndex;
+			this.elementValue = elementValue;
+		}
+		public int getElementIndex() {
+			return elementIndex;
+		}
+		public int getElementValue() {
+			return elementValue;
+		}
+		
+	}
+	
+	private static class ElementComparator implements Comparator<Element> {
+
+		@Override
+		public int compare(Element element1, Element element2) {
+			
+			if (element1.getElementValue() == element2.getElementValue()) {
+				return 0;
+			} else if (element1.getElementValue() < element2.getElementValue()) {
+				return -1;
+			} else {
+				return 1;
+			}
+
+		}
 		
 	}
 
--- QuickSortClient.java
+++ QuickSortClient.java
@@ -1,11 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class QuickSortClient {
+	
+	public static final int ITERATION_COUNT = 5;
 
 	public static void main(String[] args) {
 		
-		int[] inputArray = {2, 8, 7, 1, 3, 5, 6, 4};
-		QuickSort.sort(inputArray, 0, inputArray.length - 1);
-		System.out.println("Done");
+		int[] arraySizes = {100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+		
+		List<Double> runTimes = new ArrayList<Double>(arraySizes.length);
+		Random randomNumberGenrator = new Random(0);
+		QuickSortClient quickSortClient = new QuickSortClient();
+		double runTime = 0;
+		
+		for (int size : arraySizes) {
+			runTime = quickSortClient.getRunTime(size, true, 0, randomNumberGenrator);
+			runTimes.add(Double.valueOf(runTime));
+			System.out.println(size + " elements sorted in average of " + runTime + " ms with Big-O constant " + quickSortClient.getBigOConstant(size, runTime) + " for single random pivot");
+		}
+		
+		int[] randomPivotMedian = {1, 2, 3};
+		for (int k : randomPivotMedian) {
+			for (int size : arraySizes) {
+				runTime = quickSortClient.getRunTime(size, false, k, randomNumberGenrator);
+				runTimes.add(Double.valueOf(runTime));
+				System.out.println(size + " elements sorted in average of " + runTime + " ms with Big-O constant " + quickSortClient.getBigOConstant(size, runTime) + " for median from " + (2 * k + 1) + " pivots");
+			}
+		}
+		
+		
+	}
+	
+	private double getRunTime(int arraySize, boolean singleRandomPivot, int pivotCandidatesParam, Random randomNumberGenrator) {
+		
+		int[] arrayToSort = new int[arraySize];
+		for (int arrayIndex = 0; arrayIndex < arraySize; ++arrayIndex) {
+			arrayToSort[arrayIndex] = randomNumberGenrator.nextInt();
+		}
+		
+		int runCounter = 0;
+		long averageRunTime = 0, startTime = 0, endTime = 0;;
+		do {
+			
+			startTime = System.currentTimeMillis();
+			QuickSort.sort(arrayToSort, 0, arrayToSort.length -1, singleRandomPivot, pivotCandidatesParam, randomNumberGenrator);
+			endTime = System.currentTimeMillis();
+			
+			averageRunTime += endTime - startTime;
+			runCounter += 1;
+			
+		} while (runCounter < ITERATION_COUNT);
+		
+		return averageRunTime / ITERATION_COUNT;
+		
+	}
+	
+	private double getBigOConstant(int arraySize, double runTime) {
+		return runTime / (arraySize * Math.log(arraySize));
 	}
 
 }
